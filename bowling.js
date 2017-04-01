1a2a64fae0c3eac494390ca89a7ea596a5fb34e3
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-31 18:07:35 -1000

    second commit with 9 frames calc

--- bowling.js
+++ bowling.js
@@ -3,11 +3,31 @@ var bowling = function (frames){
 var score = 0;
 
   for(var i = 0; i < frames.length; i++){
-    score += frames[i].ball1 + frames[i].ball2;
+    var firstBall = frames[i].ball1;
+    var secondBall = frames[i].ball2;
+    var frameTotal = firstBall + secondBall;
+    var nextFirstBall = null;
+    var nextSecondBall = null;
 
-  }
+    if(i+1 >= frames.length){
+      nextFirstBall = 0;
+    }else{
+      nextFirstBall = frames[i+1].ball1;
+      nextSecondBall = frames[i+1].ball2;
+    }
+
+
+    if(frames[i].ball1 <=9 && frames[i].ball2 <=10 && frameTotal === 10) {
+      frameTotal += nextFirstBall;
+    } else if (frames[i].ball1 === 10 && frames[i].ball2 <=9 && frameTotal === 10){
+      frameTotal += nextFirstBall + nextSecondBall;
+    }
+
+    score += frameTotal;
+
+    }
 
 return score;
-console.log("here is the score",score);
+
 };
 module.exports = bowling;
--- test/add.spec.js
+++ test/add.spec.js
@@ -18,6 +18,38 @@ describe('bowling',() => {
     expect(bowling([{ball1:0,ball2:10}])).to.be.equal(10);
   });
 
+  it('should be a score a multiple frames', () =>{
+    //2 frames
+    expect(bowling([{ball1:3,ball2:4},{ball1:3,ball2:4}])).to.be.equal(14);
+      //10 frames
+    expect(bowling([{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4},{ball1:3,ball2:4}])).to.be.equal(70);
+      });
+
+  it('spare (total of 10) in a frame will add a bonus of the next first ball of the next frame', () =>{
+  //2 frames with a spare, add only 3 balls.
+  expect(bowling([{ball1:7,ball2:3},{ball1:4,ball2:2}])).to.be.equal(20);
+  });
+
+  it('strike on ball2 should be a spare', () =>{
+  //2 frames with a spare, add only 3 balls.
+  expect(bowling([{ball1:0,ball2:10},{ball1:4,ball2:2}])).to.be.equal(20);
+  });
+
+  it('strike on ball1 will should give next ball1 & ball2 bonus up to 9th frame', () =>{
+  //strike with 3 balls
+  expect(bowling([{ball1:10,ball2:0},{ball1:4,ball2:2}])).to.be.equal(22);
+  });
+
+  it('every strike should return ball1 & ball2 bonus up to the 9th frame', () =>{
+  //2 strikes, 5 frames
+  expect(bowling([{ball1:10,ball2:0},{ball1:4,ball2:2},{ball1:4,ball2:5},{ball1:10,ball2:0},{ball1:0,ball2:0}])).to.be.equal(41);
+  });
+
+  it('every strike should return ball1 & ball2 bonus and spares should return ball1 bonus up to the 9th frame', () =>{
+  //2 strikes, 2 spares 9 frames
+  expect(bowling([{ball1:10,ball2:0},{ball1:4,ball2:2},{ball1:4,ball2:5},{ball1:10,ball2:0},{ball1:0,ball2:0},{ball1:8,ball2:2},{ball1:6,ball2:3},{ball1:9,ball2:1},{ball1:2,ball2:3}])).to.be.equal(83);
+  });
+
 
 
 
